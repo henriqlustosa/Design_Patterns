d47d97b55bf462a4c322fadf99186ecb1826d89a
henriqlustosa <henriqlustosa@gmail.com>
2020-10-02 15:41:27 -0300

    Situacao problema: muitos if e else

---
+++ Design_Patterns/Design_Patterns/CalculadorDeImpostos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Design_Patterns
+{
+    public class CalculadorDeImpostos
+    {
+        public void RealizaCalculo(Orcamento orcamento, String imposto)
+        {
+            if ("ICMS".Equals(imposto))
+            {
+                double icms = orcamento.Valor * 0.1;
+                Console.WriteLine(icms);
+            }
+            else if ("ISS".Equals(imposto))
+            {
+                double iss = orcamento.Valor * 0.06;
+                Console.WriteLine(iss);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Design_Patterns/Design_Patterns/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Design_Patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double valor = 250.60;
+            Orcamento orcamento = new Orcamento(valor);
+
+            CalculadorDeImpostos calculadora = new CalculadorDeImpostos();
+
+            calculadora.RealizaCalculo(orcamento, "ISS");
+        }
+    }
+}
